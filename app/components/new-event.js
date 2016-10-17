602158fb1d7d1070f93746f218ddd2d6c63a414b
Sullivan <sullivan1982@gmail.com>
2016-10-18 00:33:04 +0800

    add fb_id to create an event

--- app/components/new-event.js
+++ app/components/new-event.js
@@ -33,19 +33,28 @@ export default Ember.Component.extend({
         var event_dates = Ember.$('#date-area').val();
         var event_desc = Ember.$('#desc-area').val();
         if (event_name === '' || event_dates === '' || event_desc === ''){
+          Ember.$('#error-msg').text('These fields cannot be blank!');
           Ember.$('#error-msg').fadeIn();
           return;
         }
+        var fb_id = sessionStorage.getItem('fb_id');
         var url = 'http://localhost:9292/events';
         Ember.$.ajax({
           url: url,
           type: 'POST',
-          data: { name: event_name, dates: event_dates, description: event_desc },
+          data: { name: event_name, dates: event_dates, description: event_desc, fb_id: fb_id },
           complete: function (data) {
             if(data['responseText']) {
-              $('#eventModal').modal('toggle');
-              var json_data =  Ember.$.parseJSON(data['responseText']);
-              $controller.sendAction('onComplete', json_data.id);
+              var json_data;
+              if (data.status === 200) {
+                Ember.$('#eventModal').modal('toggle');
+                json_data =  Ember.$.parseJSON(data['responseText']);
+                $controller.sendAction('onComplete', json_data.id);
+              } else {
+                json_data =  Ember.$.parseJSON(data['responseText']);
+                Ember.$('#error-msg').text(json_data.error);
+                Ember.$('#error-msg').fadeIn();
+              }
             }
           }
         });
