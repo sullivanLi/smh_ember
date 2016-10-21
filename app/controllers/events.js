44c4d4267dfe893f807574a5b49230f7b8ad0764
Sullivan <sullivan1982@gmail.com>
2016-10-21 17:55:25 +0800

    events delete function

---
+++ app/controllers/events.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  init: function() {
+    Ember.run.schedule("afterRender",this,function() {
+      var $controller = this;
+      Ember.$("#confirmDelete").on( "click", function() {
+        var event_id = Ember.$(this).data("eventid");
+        $controller.send('delete_event_record', event_id);
+      });
+    });
+  },
+  actions: {
+    delete_event(event) {
+      Ember.$("#confirmDelete").data("eventid", event.id);
+      Ember.$("#eventName").text(event.get("name"));
+      Ember.$('#error-msg').hide();
+      Ember.$("#deleteModal").modal('show');
+    }
+  }
+});
--- app/routes/events.js
+++ app/routes/events.js
@@ -4,5 +4,26 @@ export default Ember.Route.extend({
   model() {
     var fb_id = sessionStorage.getItem('fb_id');
     return this.store.query('event', { fb_id: fb_id });
+  },
+  actions: {
+    delete_event_record(event_id) {
+      Ember.$('#error-msg').hide();
+      var $route = this;
+      var fb_id = sessionStorage.getItem('fb_id');
+      Ember.$.ajax({
+        url: 'http://localhost:9292/events/'+event_id,
+        type: 'DELETE',
+        data: { fb_id: fb_id },
+        complete: function (data) {
+          if(data.status === 200) {
+            Ember.$("#deleteModal").modal('hide');
+            $route.refresh();
+          } else {
+            Ember.$('#error-msg').text('some errors occurred');
+            Ember.$('#error-msg').fadeIn();
+          }
+        }
+      });
+    }
   }
 });
